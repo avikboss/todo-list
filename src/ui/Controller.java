419ef3ea8acb768754af935487bfbff22dc72eef
avikboss <abosshar@macalester.edu>
2019-03-24 19:17:48 -0500

    basic ui testing

--- src/ui/Controller.java
+++ src/ui/Controller.java
@@ -1,15 +1,37 @@
 package ui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.RowConstraints;
 
 public class Controller {
+
+    private int numTodo;
 
     @FXML
     private FlowPane mainPane;
 
     @FXML
-    public void initialize() {
+    private Button addButton;
 
+    @FXML
+    private GridPane todoGrid;
+
+    @FXML
+    private void addTodo() {
+        Label added = Math.random()<0.5 ? new Label("Hello\n\nTest\n\nTest") : new Label("smol hello");
+        todoGrid.getRowConstraints().add(numTodo,new RowConstraints(added.getPrefHeight()));
+        todoGrid.add(added,0,numTodo);
+        numTodo++;
+    }
+
+    @FXML
+    public void initialize() {
+        todoGrid.setGridLinesVisible(true);
+        numTodo = 0;
     }
 }
